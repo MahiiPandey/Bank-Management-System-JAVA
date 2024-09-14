da727c8b79e2248432500048d97ce345350d1f05
darhitsorathiya <darhitsorthiya@gmail.com>
2024-09-14 10:37:12 +0530

    updated

--- src/main/java/com/miniproject/Bank/DataBase/DatabaseConnection.java
+++ src/main/java/com/miniproject/Bank/DataBase/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/miniproject";
     private static final String USER = "root";
-    private static final String PASSWORD = "darshit89";
+    private static final String PASSWORD = "Mahi@2708";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
